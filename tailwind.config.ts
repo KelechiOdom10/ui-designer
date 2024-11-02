3d5344bb0ef6e9d65bc2d02e4d9be06e9cd57f99
Kelechi Odom <kelechiodom10@gmail.com>
2024-11-02 19:50:34 +0000

    feat: set up skeleton ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,23 @@
-import forms from '@tailwindcss/forms';
-import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import type { Config } from "tailwindcss";
+
+import { join } from "path";
+import { skeleton } from "@skeletonlabs/skeleton/plugin";
+import * as themes from "@skeletonlabs/skeleton/themes";
 
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-
+  content: [
+    "./src/**/*.{html,js,svelte,ts}",
+    join(
+      require.resolve("@skeletonlabs/skeleton-svelte"),
+      "../**/*.{html,js,svelte,ts}"
+    ),
+  ],
   theme: {
-    extend: {}
+    extend: {},
   },
-
-  plugins: [typography, forms]
+  plugins: [
+    skeleton({
+      themes: [themes.cerberus, themes.nouveau],
+    }),
+  ],
 } satisfies Config;
